b08f4797b3d6c105d7b6df00a421109df6e1af44
Hamidreza Soleimani <hamidreza.s@gmail.com>
2015-12-10 18:34:21 +0330

    modify c/network_interfaces_1 and add c/network_interfaces_2

--- C/networking/network_interfaces_1.c
+++ C/networking/network_interfaces_1.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <ifaddrs.h>
 #include <stdio.h>
+#include <string.h>
 
 /* 
    @NOTE:
@@ -13,25 +14,19 @@ int main(void)
   struct ifaddrs *ifap, *ifa;
   struct sockaddr_in *sa;
   struct sockaddr_in *bc;
-  char *sa_addr;
-  char *bc_addr;
   int rc;
-  
+
   getifaddrs(&ifap);
   for(ifa = ifap; ifa; ifa = ifa->ifa_next) {
 
     if(ifa->ifa_addr->sa_family == AF_INET) {
-
+     
+      bc = (struct sockaddr_in *) ifa->ifa_broadaddr;
       sa = (struct sockaddr_in *) ifa->ifa_addr;
-      sa_addr = inet_ntoa(sa->sin_addr);
- 
-      bc = (struct sockaddr_in *) /* ifa->ifa_dstaddr; */ ifa->ifa_broadaddr;
-      bc_addr = inet_ntoa(bc->sin_addr);
-
-      if(rc = (strncmp(ifa->ifa_name, "en0", 3) == 0)) {
-	printf("rc: %d\n", rc);
-      	printf("Interface: %s\t\t\tAddress: %s", ifa->ifa_name, sa_addr);
-	printf("\t\t\tBroadcast: %s\n", bc_addr);
+      
+      if((rc = strncmp(ifa->ifa_name, "en0", 3)) == 0) {
+      	printf("Interface: %s\t\t\tAddress: %s", ifa->ifa_name, inet_ntoa(sa->sin_addr));
+	printf("\t\t\tBroadcast: %s\n", inet_ntoa(bc->sin_addr));
 	break;
       }
       
