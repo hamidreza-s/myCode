0cbe30677e3e615a556f30fb7ee6871822566401
Hamidreza Soleimani <hamidreza.s@gmail.com>
2017-01-29 18:56:42 +0330

    add progression classes for Java

--- Java/src/main/java/App.java
+++ Java/src/main/java/App.java
@@ -1,11 +1,5 @@
 public class App {
     public static void main(String[] args) {
 
-        algorithm.Utils.print(algorithm.Utils.welcome());
-
-        algorithm.unionfind.QuickFind.test();
-        algorithm.unionfind.QuickUnion.test();
-        algorithm.unionfind.WeightedQuickUnion.test();
-
     }
 }
---
+++ Java/src/main/java/misc/ArithmeticProgression.java
@@ -0,0 +1,12 @@
+public class ArithmeticProgression extends Progression {
+    protected long increment;
+    public ArithmeticProgression() { this(1, 0); }
+    public ArithmeticProgression(long stepsize) { this(stepsize, 0); }
+    public ArithmeticProgression(long stepsize, long start) {
+	super(start);
+	increment = stepsize;
+    }
+    protected void advance() {
+	current += increment;
+    }
+}
