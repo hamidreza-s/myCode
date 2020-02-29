6df5ffe48d474f7e048394f70ad6ffb3b8b0c9bc
Hamidreza Soleimani <hamidrezas@spotify.com>
2020-03-01 00:34:57 +0100

    Add functional Scala LinkedList

--- Scala/DataStructure/List.scala
+++ Scala/DataStructure/List.scala
@@ -1,60 +1,37 @@
-class LinkedList[T] {
+abstract sealed class List[+A] {
+  def head: A
+  def tail: List[A]
+  def isEmpty: Boolean
 
-  import scala.collection.mutable.ListBuffer
+  def prepend[B >: A](x: B): List[B] = Cons(x, this)
 
-  case class Node[T](var value: T, var next: Option[Node[T]])
-  
-  var head: Option[Node[T]] = None
+  def append[B >: A](x: B): List[B] =
+    if (isEmpty) Cons(x)
+    else Cons(head, tail.append(x))
 
-  def add(value: T) {
-    val cache = head
-    this.head = Some(Node(value, cache))
+  def apply(n: Int): A =
+    if (isEmpty) throw new Exception("Index out of range")
+    else if (n == 0) head
+    else tail(n - 1)
+}
+
+case object Nil extends List[Nothing] {
+  def head: Nothing = throw new Exception("empty list")
+  def tail: Nothing = throw new Exception("empty list")
+  def isEmpty: Boolean = true
+}
+
+case class Cons[A](head: A, tail: List[A] = Nil) extends List[A] {
+  def isEmpty: Boolean = false
+}
+
+case object List {
+  def empty[A]: List[A] = Nil
+  def apply[A](xs: A*): List[A] = create(xs.reverse, empty)
+
+  @scala.annotation.tailrec
+  private def create[A](xs: Seq[A], acc: List[A]): List[A] = xs match {
+    case head +: tail => create(tail, acc.prepend(head))
+    case Seq() => acc
   }
-
-  def delete(value: T): Unit = {
-
-    if (head.isEmpty) return
-
-    if (head.get.value == value) {
-      head = head.get.next
-      return
-    }
-
-    var curr = head
-
-    while (curr.get.next.isDefined) {
-      if (curr.get.next.get.value == value) {
-        curr.get.next = curr.get.next.flatMap(_.next)
-        return
-      }
-      curr = curr.get.next
-    }
-
-  }
-
-  def toList: List[T] = {
-    @scala.annotation.tailrec
-    def loop(self: Option[Node[T]], acc: List[T]): List[T] = {
-      self match {
-        case Some(Node(value, Some(next))) => loop(Some(next), value :: acc)
-        case Some(Node(value, None)) => value :: acc
-        case None => acc
-      }
-    }
-    loop(head, Nil)
-  }
-  
-  def toListBuffer: ListBuffer[T] = {
-    var current = head
-    val result = ListBuffer.empty[T]
-
-    while (current.isDefined) {
-      result.append(current.get.value)
-      current = current.get.next
-    }
-
-    result
-  }
-
-  override def toString: String = toList.toString
 }
