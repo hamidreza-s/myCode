61a130a48f1e2c75b83ecbdc357a41d3a3fb743e
Hamidreza Soleimani <hamidrezas@spotify.com>
2020-03-01 16:05:47 +0100

    Add reverse and concat to LinkedList

--- Scala/DataStructure/List.scala
+++ Scala/DataStructure/List.scala
@@ -1,23 +1,60 @@
+package functional.data.structure
+
+import scala.annotation.tailrec
+
 abstract sealed class List[+A] {
   def head: A
   def tail: List[A]
   def isEmpty: Boolean
 
+  /**
+   * Time: O(1)
+   * Space: O(1)
+   */
   def prepend[B >: A](x: B): List[B] = Cons(x, this)
 
+  /**
+   * Time: O(n)
+   * Space: O(n)
+   */
   def append[B >: A](x: B): List[B] =
     if (isEmpty) Cons(x)
     else Cons(head, tail.append(x))
 
+  /**
+   * Time: O(n)
+   * Space: O(n)
+   */
   def apply(n: Int): A =
     if (isEmpty) throw new Exception("Index out of range")
     else if (n == 0) head
     else tail(n - 1)
+
+  /**
+   * Time: O(n)
+   * Space: O(n)
+   */
+  def concat[B >: A](xs: List[B]): List[B] =
+    if (isEmpty) xs
+    else tail.concat(xs).prepend(head)
+
+  /**
+   * Time: O(n)
+   * Space: O(n)
+   */
+  def reverse: List[A] = {
+    @tailrec
+    def loop(s: List[A], d: List[A]): List[A] =
+      if (s.isEmpty) d
+      else loop(s.tail, d.prepend(s.head))
+    loop(this, Nil)
+  }
+
 }
 
 case object Nil extends List[Nothing] {
-  def head: Nothing = throw new Exception("empty list")
-  def tail: Nothing = throw new Exception("empty list")
+  def head: Nothing = throw new Exception("Empty list")
+  def tail: Nothing = throw new Exception("Empty list")
   def isEmpty: Boolean = true
 }
 
@@ -29,7 +66,7 @@ case object List {
   def empty[A]: List[A] = Nil
   def apply[A](xs: A*): List[A] = create(xs.reverse, empty)
 
-  @scala.annotation.tailrec
+  @tailrec
   private def create[A](xs: Seq[A], acc: List[A]): List[A] = xs match {
     case head +: tail => create(tail, acc.prepend(head))
     case Seq() => acc
